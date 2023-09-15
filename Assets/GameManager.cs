75c7f20badc209506dbf797b032d9d00999004a2
Ilya Dubov <dubov.study37@outlook.com>
2023-09-15 21:14:30 +0300

    Debug

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,58 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Management;
+
+using UnityEngine.XR;
+using Valve.VR;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        StartXR();
+        Debug.Log(OpenVR.IsHmdPresent().ToString());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+
+	// Update is called once per frame
+	void Update()
+        {
         
-    }
-
-    public IEnumerator StartXR()
-    {
-        Debug.Log("Initializing XR...");
-        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
-
-        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
-        {
-            Debug.LogError("Initializing XR Failed. Directing to Normal Interaciton Mode...!.");
-            StopXR();
-            DirectToNormal();
         }
-        else
-        {
-            Debug.Log("Initialization Finished.Starting XR Subsystems...");
 
-            //Try to start all subsystems and check if they were all successfully started ( thus HMD prepared).
-            bool loaderSuccess = XRGeneralSettings.Instance.Manager.activeLoader.Start();
-            if (loaderSuccess)
-            {
-                Debug.Log("All Subsystems Started!");
-            }
-            else
-            {
-                Debug.LogError("Starting Subsystems Failed. Directing to Normal Interaciton Mode...!");
-                StopXR();
-                DirectToNormal();
-            }
-        }
     }
-
-    void StopXR()
-    {
-            Debug.Log("XR stopped completely.");
-    }
-    void DirectToNormal()
-    {
-            Debug.Log("Fell back to Mouse & Keyboard Interaciton!");
-    }
-}
