39f97d33e4b37c2c12bca7fcf0800ceacd3c092c
Dolbozavr <maksim.milyukov@mail.ru>
2023-09-21 21:04:16 +0300

    Upper
    Апдейтнул текстуру, добавил лампу, добавил полку, доделал чела, исправил часть багов.
    Добавил отключение курсора на ESC и TAB добавление

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -26,11 +26,10 @@ public class Movement : MonoBehaviour
     private Vector3 needpos;
     private Vector3 needpos3d;
     private Vector3 basepos;
+    private bool work=false;
     bool isGrounded; //Проверка на землю
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // Отключаем наш указатель при игре, ШОБ НЕ МЕШАЛСЯ
-        Cursor.visible = false;
         anim = GetComponent<Animator>();
         CharacterController = GetComponent<CharacterController>(); // Получаем, собственно, сам объект
         if (CharacterController == null) //Проверка его наличия (объекта)
@@ -79,7 +78,18 @@ public class Movement : MonoBehaviour
         {
             velocity.y = -2f;
         }
-        if(Input.GetKey(KeyCode.Alpha3)) 
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.Locked; // Отключаем наш указатель при игре, ШОБ НЕ МЕШАЛСЯ
+            Cursor.visible = false;
+
+        }
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            Cursor.lockState = CursorLockMode.Confined; // Отключаем наш указатель при игре, ШОБ НЕ МЕШАЛСЯ
+            Cursor.visible = true;
+        }
+        if (Input.GetKey(KeyCode.Alpha3)) 
         {
             needpos3d = new Vector3(0f, 2.063f, -3.379f);
             Kamera1.transform.localPosition = Vector3.Lerp(a: basepos, b: needpos3d, t: 5f);
