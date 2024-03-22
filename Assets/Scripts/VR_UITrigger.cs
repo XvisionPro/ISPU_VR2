e9fc3bf2edc6275a18b529f20c332dab2bf973f7
Ilya <101048573+XvisionPro@users.noreply.github.com>
2024-03-22 14:51:26 +0300

    UI part almost Done
    Почти доделан универсальная часть UI для приборов.

--- Assets/Scripts/VR_UITrigger.cs
+++ Assets/Scripts/VR_UITrigger.cs
@@ -1,5 +1,6 @@
 using Invector.vCamera;
 using Invector.vCharacterController;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,33 @@ public class VR_UITrigger : MonoBehaviour
 {
     public static vThirdPersonInput inputInstance;
     public static vThirdPersonCamera cameraInstance;
+
     private Canvas[] canvases;
+    public Camera[] cameras;
+    private GenericInput cameraSwitch = new GenericInput("Switch", "", "");
+    private int _currentCamera;
+    public int currentCamera 
+    {
+        get
+        {
+            return _currentCamera;
+        }
+        set {
+            if(_currentCamera + value > cameras.Length)
+            {
+                _currentCamera = cameras.Length - (value);
+            }
+            if(_currentCamera + value < 0)
+            {
+                _currentCamera = cameras.Length - 1;
+            }
+            else { 
+                _currentCamera = 0; // HACK: По сути не нужно
+            }
+        }
+    }
+
+
     public UnityEvent OnInteractEvent;
 
     void Awake()
@@ -20,8 +47,37 @@ public class VR_UITrigger : MonoBehaviour
         {
             item.enabled = false;
         }
+        foreach (var item in cameras)
+        {
+            item.enabled = false;
+        }
     }
 
+    private void Update()
+    {
+        if (inputInstance == null || Time.timeScale == 0)
+        {
+            return;
+        }
+
+        inputHandler();
+    }
+
+    private void inputHandler()
+    {
+        if (inputInstance.isAction && cameraSwitch.GetButtonDown())
+        {
+            Debug.Log(cameraSwitch.GetAxis());
+            if (cameraSwitch.GetAxis() > 0) //TODO: Сделать так, чтоб в обе стороны была циклическая смена камеры
+            {
+                changeCamera(false);
+            }
+            else
+            {
+                changeCamera(true);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -62,7 +118,14 @@ public class VR_UITrigger : MonoBehaviour
         }
     }
 
-    public void SetCameraToObject()
+    private void changeCamera(bool isReverse)
+    {
+        cameras[currentCamera].enabled = !cameras[currentCamera].enabled;
+        currentCamera += isReverse ? -1 : 1; //TODO: Переделать на Axis 
+        cameras[currentCamera].enabled = !cameras[currentCamera].enabled;
+    }
+
+    public void FreezeMovement()
     {
         if (inputInstance.isAction)
         {
@@ -70,6 +133,7 @@ public class VR_UITrigger : MonoBehaviour
             inputInstance.LockCursor(true);
             inputInstance.ShowCursor(true);
             inputInstance.SetLockCameraInput(true);
+            inputInstance.lockMoveInput = true;
         }
         else
         {
@@ -77,6 +141,26 @@ public class VR_UITrigger : MonoBehaviour
             inputInstance.LockCursor(false);
             inputInstance.ShowCursor(false);
             inputInstance.SetLockCameraInput(false);
+            inputInstance.lockMoveInput = false;
+        }
+    }
+
+    public void SetCameraToObject()
+    {
+        if(inputInstance.isAction && cameras.Length > 0)
+        {
+            cameraInstance.targetCamera.enabled = !cameraInstance.targetCamera.enabled;
+            cameras[0].enabled = !cameras[0].enabled; //Первая смена камеры.
+            currentCamera = 0;
+
+        }
+        else
+        {
+            cameraInstance.targetCamera.enabled = true;
+            foreach (var item in cameras)
+            {
+                item.enabled = false;
+            }
         }
     }
 }
